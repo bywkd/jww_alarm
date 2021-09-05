973105fb1616a57dfba2b715c5aa81f20af25ca7
bywkd <cd100go@naver.com>
2021-09-05 14:58:20 +0900

    최초 커밋: 프로젝트 생성

---
+++ app/src/main/java/com/jww/alarm/bases/BaseActivity.kt
@@ -0,0 +1,6 @@
+package com.jww.alarm.bases
+
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity : AppCompatActivity() {
+}
---
+++ app/src/main/java/com/jww/alarm/bases/BaseFragment.kt
@@ -0,0 +1,6 @@
+package com.jww.alarm.bases
+
+import androidx.fragment.app.Fragment
+
+open class BaseFragment : Fragment() {
+}
