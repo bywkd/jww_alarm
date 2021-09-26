88b92a12ad769ba83b80c62937a2587cd25f278b
bywkd <cd100go@naver.com>
2021-09-26 19:02:04 +0900

    alarm-0003: 1. 알람 아이템 모델 수정, 2. 알람 등록 화면 UI 와 초기 데이터 연결 및 기능 구현

--- app/src/main/java/com/jww/alarm/views/registerAlarmView/RegisterAlarmFragment.kt
+++ app/src/main/java/com/jww/alarm/views/registerAlarmView/RegisterAlarmFragment.kt
@@ -1,6 +1,7 @@
 package com.jww.alarm.views.registerAlarmView
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,6 +25,7 @@ class RegisterAlarmFragment : BaseFragment() {
     private var _binding: FragmentRegisterAlarmBinding? = null
     private val binding
         get() = _binding!!
+
     private lateinit var vm: RegisterAlarmVM
 
     override fun onCreateView(
@@ -38,5 +40,61 @@ class RegisterAlarmFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        init()
+    }
+
+    private fun init() {
+        uiDataInit()
+        bind()
+        observer()
+    }
+
+    private fun uiDataInit() {
+        vm.uiInitSound(false)
+            .uiInitVibration(true)
+            .uiInitTime(binding.timePicker, 6, 30)
+            .uiInitCalendar(binding.calendar, 2021, 3, 20)
+    }
+
+    private fun bind() {
+        binding.llSound.setOnClickListener {
+            vm.setSound()
+        }
+
+        binding.llVibration.setOnClickListener {
+            vm.setVibration()
+        }
+
+        binding.ivRegister.setOnClickListener {
+            vm.completeRegister()
+        }
+
+        binding.timePicker.setOnTimeChangedListener(vm.timerListener)
+        binding.calendar.setOnDateChangeListener(vm.calendarListener)
+    }
+
+    private fun observer() {
+        vm.isSound.observe(viewLifecycleOwner, {
+            if (it) {
+//                온 이미지
+            } else {
+//                오프 이미지
+            }
+            Log.d("Won", "isSound = $it")
+        })
+
+        vm.isVibration.observe(viewLifecycleOwner, {
+            if (it) {
+//                온 이미지
+            } else {
+//                오프 이미지
+            }
+            Log.d("Won", "isVibration = $it")
+        })
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
